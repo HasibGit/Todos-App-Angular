da92e37af5c2d779ede9f2c9968232a8d4d42025
HasibGit <48997902+HasibGit@users.noreply.github.com>
2022-01-17 21:54:39 +0600

    Created Todo Model

--- src/app/MyComponents/todos/todos.component.ts
+++ src/app/MyComponents/todos/todos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Todo } from '../../Todo';
 
 @Component({
   selector: 'app-todos',
@@ -7,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodosComponent implements OnInit {
 
+  todos: Todo[];
   constructor() { }
 
   ngOnInit(): void {
